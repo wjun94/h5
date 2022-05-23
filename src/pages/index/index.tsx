7de81a8dd57dd3a211833d40308e31117f39a4ee
wjun94 <994037715@qq.com>
2022-05-23 20:55:20 +0800

    feat: 添加新页面

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -20,14 +20,25 @@ export default class Index extends Component {
   componentDidHide() { }
 
   render() {
+    const type = window.location.hash.split('=')[1]
+    const txt = { date: '2021-2-22', label: '贵州习酒.御藏', info: '酒瓶' }
+    if (type === '1') {
+      txt.date = '2021年12月18 '
+      txt.label = '贵州习酒.御品'
+      txt.info = '酒盒'
+    } else if (type === '2') {
+      txt.date = '2021-2-22 '
+      txt.label = '贵州习酒.御藏'
+      txt.info = '酒盒'
+    }
     return (
       <Flex direction='col' className='index'>
         <Flex className='head'>
           <Image className='full' mode='aspectFit' src={OneJpeg} />
           <Flex className='content' direction='col'>
-            <Typography.Text type='white' size='xs'>名称：贵州习酒.御藏</Typography.Text>
-            <Typography.Text type='white' size='xs'>生产日期：2021-2-22</Typography.Text>
-            <Typography.Text type='white' size='xs'>酒瓶防伪码：062500182099345348XXXX</Typography.Text>
+            <Typography.Text type='white' size='xs'>名称：{txt.label}</Typography.Text>
+            <Typography.Text type='white' size='xs'>生产日期：{txt.date}</Typography.Text>
+            <Typography.Text type='white' size='xs'>{txt.info}防伪码：062500182099345348XXXX</Typography.Text>
             <Typography.Text onClick={() => navigateTo({ url: '/pages/detail/index' })} className='txt' type='white'>该产品为<Typography.Text className='info'>真品</Typography.Text>，请放心引用，如有疑问请<Typography.Text className='info'>进一步验证</Typography.Text>。</Typography.Text>
           </Flex>
         </Flex>
