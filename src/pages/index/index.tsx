2ab465b424ed1c1ce2212746684e61f5a095db4a
项文俊 <wj.xiang>
2022-05-30 10:36:36 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -11,13 +11,13 @@ import './index.less'
 export default () => {
   const [visible, setVisible] = useState(false);
   const type = window.location.hash.split('=')[1]
-  const txt = { date: '2021年12月18日', label: '贵州习酒.御藏', info: '酒瓶' }
+  const txt = { date: '2021年12月21日', label: '贵州习酒.御藏', info: '酒瓶' }
   if (type === '1') {
-    txt.date = '2021年12月18 '
+    txt.date = '2021年12月16日'
     txt.label = '贵州习酒.御品'
     txt.info = '酒盒'
   } else if (type === '2') {
-    txt.date = '2021-2-22 '
+    txt.date = '2021年12月18日'
     txt.label = '贵州习酒.御藏'
     txt.info = '酒盒'
   }
